7cae681adcaef7f1398dc4b4b8a9ab253303543d
Arda <96637948+verfired8975@users.noreply.github.com>
2022-03-09 02:43:20 +0300

    Add files via upload

---
+++ VIVA UI/VIVA UI/Animations.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace VIVA_UI
+{
+    public class Animations
+    {
+        public IEasingFunction CreateBounceAnimation(int Bounces, int Bounciness)
+        {
+            return new BounceEase { Bounces = Bounces, Bounciness = Bounciness, EasingMode = EasingMode.EaseInOut };
+        }
+        public IEasingFunction CreateCircleAnimation()
+        {
+            return new CircleEase { EasingMode = EasingMode.EaseInOut };
+        }
+        public IEasingFunction CreateCubicAnimation()
+        {
+            return new CubicEase { EasingMode = EasingMode.EaseInOut };
+        }
+        public IEasingFunction CreateElasticAnimation(int Oscillations, int Springiness)
+        {
+            return new ElasticEase { EasingMode = EasingMode.EaseInOut, Oscillations = Oscillations, Springiness = Springiness };
+        }
+        public IEasingFunction CreateExponentialAnimation(int Exponent)
+        {
+            return new ExponentialEase { EasingMode = EasingMode.EaseInOut, Exponent = Exponent };
+        }
+        public IEasingFunction CreatePowerAnimation(int Power)
+        {
+            return new PowerEase { EasingMode = EasingMode.EaseInOut, Power = Power };
+        }
+        public IEasingFunction CreateQuadraticAnimation()
+        {
+            return new QuadraticEase { EasingMode = EasingMode.EaseInOut };
+        }
+        public IEasingFunction CreateQuarticAnimation()
+        {
+            return new QuarticEase { EasingMode = EasingMode.EaseInOut };
+        }
+        public IEasingFunction CreateQuinticAnimation()
+        {
+            return new QuinticEase { EasingMode = EasingMode.EaseInOut };
+        }
+        public IEasingFunction CreateSineAnimation()
+        {
+            return new SineEase { EasingMode = EasingMode.EaseInOut };
+        }
+    }
+    public class Actions
+    {
+        public Task DoDoubleAnimation(DependencyObject Object, double From, double To, PropertyPath Property, IEasingFunction Animation, int Duration)
+        {
+            var sb = new Storyboard();
+            var anim = new DoubleAnimation()
+            {
+                From = From,
+                To = To,
+                Duration = new Duration(TimeSpan.FromMilliseconds(Duration)),
+                EasingFunction = Animation,
+            };
+
+            Storyboard.SetTarget(anim, Object);
+            Storyboard.SetTargetProperty(anim, Property);
+            sb.Children.Add(anim);
+            sb.Begin();
+            return Task.CompletedTask;
+        }
+        public Task DoThicknessAnimation(DependencyObject Object, Thickness From, Thickness To, PropertyPath Property, IEasingFunction Animation, int Duration)
+        {
+            var sb = new Storyboard();
+            var anim = new ThicknessAnimation()
+            {
+                From = From,
+                To = To,
+                Duration = new Duration(TimeSpan.FromMilliseconds(Duration)),
+                EasingFunction = Animation,
+            };
+
+            Storyboard.SetTarget(anim, Object);
+            Storyboard.SetTargetProperty(anim, Property);
+            sb.Children.Add(anim);
+            sb.Begin();
+
+            return Task.CompletedTask;
+        }
+        public Task DoSizeAnimation(DependencyObject Object, Size From, Size To, PropertyPath Property, IEasingFunction Animation, int Duration)
+        {
+            var sb = new Storyboard();
+            var anim = new SizeAnimation()
+            {
+                From = From,
+                To = To,
+                Duration = new Duration(TimeSpan.FromMilliseconds(Duration)),
+                EasingFunction = Animation,
+            };
+
+            Storyboard.SetTarget(anim, Object);
+            Storyboard.SetTargetProperty(anim, Property);
+            sb.Children.Add(anim);
+            sb.Begin();
+
+            return Task.CompletedTask;
+        }
+        public Task DoColorAnimation(DependencyObject Object, Color From, Color To, PropertyPath Property, IEasingFunction Animation, int Duration)
+        {
+            var sb = new Storyboard();
+            var anim = new ColorAnimation()
+            {
+                From = From,
+                To = To,
+                Duration = new Duration(TimeSpan.FromMilliseconds(Duration)),
+                EasingFunction = Animation,
+            };
+
+            Storyboard.SetTarget(anim, Object);
+            Storyboard.SetTargetProperty(anim, Property);
+            sb.Children.Add(anim);
+            sb.Begin();
+
+            return Task.CompletedTask;
+        }
+        public Task DoRectangleAnimation(DependencyObject Object, Rect From, Rect To, PropertyPath Property, IEasingFunction Animation, int Duration)
+        {
+            var sb = new Storyboard();
+            var anim = new RectAnimation()
+            {
+                From = From,
+                To = To,
+                Duration = new Duration(TimeSpan.FromMilliseconds(Duration)),
+                EasingFunction = Animation,
+            };
+
+            Storyboard.SetTarget(anim, Object);
+            Storyboard.SetTargetProperty(anim, Property);
+            sb.Children.Add(anim);
+            sb.Begin();
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ VIVA UI/VIVA UI/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace VIVA_UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public TabControl Tab = null;
+
+        private void AddTab(object sender, RoutedEventArgs e)
+        {
+            VIVA_Functions.AddTabFunction(Tab, ("Script " + (Tab.Items.Count + 1)), "");
+        }
+
+        private void Start(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                var VIVA = new VIVA();
+                VIVA.Show();
+                Tab = VIVA.Tabs;
+                AddTab(null, null);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.DialogResult Result = System.Windows.Forms.MessageBox.Show("An error Has occured, would you like to copy the error to your clipboard?", "VIVA Error Handler.", System.Windows.Forms.MessageBoxButtons.YesNo, System.Windows.Forms.MessageBoxIcon.Error, System.Windows.Forms.MessageBoxDefaultButton.Button1);
+                if (Result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    Clipboard.SetText(ex.ToString());
+                    System.Diagnostics.Process.GetCurrentProcess().Kill();
+                }
+            }
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            System.Windows.Forms.DialogResult Result = System.Windows.Forms.MessageBox.Show("An error Has occured, would you like to copy the error to your clipboard?", "VIVA Error Handler.", System.Windows.Forms.MessageBoxButtons.YesNo, System.Windows.Forms.MessageBoxIcon.Error, System.Windows.Forms.MessageBoxDefaultButton.Button1);
+            e.Handled = true;
+        }
+    }
+}
